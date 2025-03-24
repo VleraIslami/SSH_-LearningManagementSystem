877cb8731efe2ed8ca84d5efcd6f5ffce54a1b7c
Vlera Islami <117523183+VleraIslami@users.noreply.github.com>
2025-03-24 16:22:44 +0100

    API documentation using DRF-YASG

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,48 @@
 from api import views as api_views
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework_simplejwt.views import TokenRefreshView
+
+
+from rest_framework import parmissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Learnin system",
+        default_version='v1',
+        terms_of_service="https://www.google.com/policies/terms",
+        contatct=openapi.Contact(email="vleraislami11@gmail.com"),
+        license=openapi.License(name="BSD License"),
+
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
+urlpatterns = [
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0),
+         name='schema-json'),
+    path('', schema_view.with_ui('swagger', cashe_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-swagger-ui'),
+
+
+
+
+
+    path('admin/', admin.site.urls),
+
+    path("api/v1/", include("api.urls"))
+
+]
+
 
 urlpatterns = [
     path("user/token/", api_views.MyTokenObtainPairView.as_view()),
