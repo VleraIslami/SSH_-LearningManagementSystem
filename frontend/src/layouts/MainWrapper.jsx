124a054f2fb1a58a6f9c1303ab050243c2bba979
Vlera Islami <117523183+VleraIslami@users.noreply.github.com>
2025-05-12 14:11:28 +0200

    changed on  frontend/src/layouts/MainWrapper.jsx

---
+++ frontend/src/layouts/MainWrapper.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { setUser } from "../utils/Auth";
+
+const MainWrapper = ({ children }) => {
+  //kur thirret "projecti" vjen krejt kodi posht, nese jo
+  const [loding, setLoading] = useState(true); //e gjejm nje variabel edhe eerdorimme set data
+
+  useEffect(() => {
+    const handler = async () => {
+      setLoading(true);
+
+      await setUser(); //nese krejt shkon okej per me u atentifiku e bojm setloadinf false
+
+      setLoading(false); //ska nevoj me set data se u krys
+    };
+    handler();
+  }, []);
+  return <>{loding ? null : children}</>; //nese loading eshte complet bohet children
+};
+
+export default MainWrapper;
